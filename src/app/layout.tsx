6351145afd514b68e8d67858f7171ef9c939a329
rebecaCaroba <rebecacaroba@gmail.com>
2023-12-12 13:03:38 -0300

    Configuração do ESLint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
 import './styles/global.scss'
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
