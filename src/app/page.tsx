68d4323f34601994524595effd875d6c21094ee9
rebecaCaroba <rebecacaroba@gmail.com>
2023-12-12 14:12:37 -0300

    Estilizando página Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import banner from '../assets/banner.svg'
+import Image from 'next/image'
+import './style.scss'
+import { FaGoogle } from "react-icons/fa";
+
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="Container">
+      <div className="text-home">
+        <h1>Agendamento descomplicado</h1>
+        <p>
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </p>
+        <button> 
+          <FaGoogle/>
+        Criar conta com Google</button>
+      </div>
+      <Image
+        src={banner}
+        alt="calendario"
+        height={400}
+        quality={100}
+        priority
+      />
     </div>
   )
 }
