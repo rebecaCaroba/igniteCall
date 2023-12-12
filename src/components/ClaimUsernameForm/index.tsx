e3ad10d2fcdb7dbbc2f33e120b9b270a2990af1c
rebecaCaroba <rebecacaroba@gmail.com>
2023-12-12 15:33:04 -0300

    Utilizando React Hook Form

--- src/components/ClaimUsernameForm/index.tsx
+++ src/components/ClaimUsernameForm/index.tsx
@@ -1,9 +1,24 @@
+'use client'
+import { useForm } from 'react-hook-form'
 import { FaCaretRight } from 'react-icons/fa'
+import { z } from 'zod'
+
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormSchema = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormSchema>()
+
+  async function handlePreRegister(data: ClaimUsernameFormSchema) {
+    console.log(data)
+  }
+
   return (
-    <form>
-      <input type="text" placeholder="seu-usuário" />
+    <form onSubmit={handleSubmit(handlePreRegister)}>
+      <input type="text" placeholder="seu-usuário" {...register('username')} />
       <button type="submit">
         <FaCaretRight />
         Reservar usuário
